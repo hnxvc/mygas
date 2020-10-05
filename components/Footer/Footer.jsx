cf0548a2564706f3afd298d0eaa2816e1db26f4a
Hoa Nguyen Xuan <hoanguyen.contact@gmail.com>
2020-10-05 11:17:11 +0700

    Style footer

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function Footer() {
   return (
-    <div>
-      Footer
-    </div>
+    <StyledFooter>
+      <StyledCopyright>
+        Copyright© 2020 MyGas. All rights reserved.
+      </StyledCopyright>
+      <StyledPower>Powered by Etherscan.io and CoinGecko APIs</StyledPower>
+    </StyledFooter>
   )
 }
+
+const StyledFooter = styled.footer`
+  display: flex;
+  justify-content: space-between;
+  text-align: center;
+`
+
+const StyledCopyright = styled.div`
+  flex-basis: 50%;
+`
+
+const StyledPower = styled.div`
+  flex-basis: 50%;
+`
 
 export default Footer
